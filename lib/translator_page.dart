93fcefb8fb86f2ff886df68c326b7d7a8519572d
Sohag <ihsohag-84@gmail.com>
2023-07-09 10:41:42 +0600

    initial commit

---
+++ lib/translator_page.dart
@@ -0,0 +1,40 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+
+class TranslatorPage extends StatefulWidget {
+  const TranslatorPage({super.key});
+
+  @override
+  State<TranslatorPage> createState() => _TranslatorPageState();
+}
+
+class _TranslatorPageState extends State<TranslatorPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 87, 104, 254),
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 55, 66, 162),
+        title: Text(
+          "Translator",
+          style: TextStyle(
+            fontSize: 28,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+            letterSpacing: 1,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Language Translator"),
+          ],
+        ),
+      ),
+    );
+  }
+}
